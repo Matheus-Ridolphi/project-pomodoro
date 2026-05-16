387583e091fd468f0a075b0ac04d1d5ab76eb260
Matheus-Ridolphi <ridolphim@gmail.com>
2026-05-16 16:02:13 -0300

    Pasta separada para cada componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
-import { Container } from "./components/container";
-import { Heading } from "./components/heading";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 
 export function App() {
   return (
